80dba22d211d6f2c72e8483177a35e2ae528a99f
Vinay <vinay.py@gmail.com>
2021-04-06 04:07:58 -0400

    First commit

---
+++ src/test/java/com/galvanize/guestBook/GuestBookApplicationTests.java
@@ -0,0 +1,13 @@
+package com.galvanize.guestBook;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GuestBookApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
